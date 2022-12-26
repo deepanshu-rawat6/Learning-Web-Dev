56de2cbf4e45501d0bac020d58bba799fb87ab13
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2022-12-26 19:24:10 +0530

    Update functionJS.js

--- Learning Part(U)/javascript/functionJS.js
+++ Learning Part(U)/javascript/functionJS.js
@@ -1,4 +1,4 @@
-function getMilk(bottles){
+function getMilk(currentBalance, costPerBottle){
     console.log("leaveHouse");
     console.log("moveRight");
     console.log("moveRight");
@@ -9,7 +9,9 @@ function getMilk(bottles){
     console.log("moveRight");
     console.log("moveRight");
     console.log("leaveHouse");
-    console.log("buy " + bottles +" bottles of milk");
+    
+    console.log("buy " + calcBottles(currentBalance, costPerBottle) +" bottles of milk");
+    // console.log("balance left: " + currentBalance)
     console.log("moveLeft");
     console.log("moveLeft");
     console.log("moveDown");
@@ -19,9 +21,24 @@ function getMilk(bottles){
     console.log("moveLeft");
     console.log("moveLeft");
     console.log("enterHouse");
+    return calcChange(currentBalance,costPerBottle);
 }
 
-getMilk(10);
+function calcBottles(startingMoney, costPerBottle) {
+    var numberOfBottles = Math.floor(startingMoney / costPerBottle);
+    
+    return numberOfBottles;
+}
+
+function calcChange(startingAmount, costPerBottle) {
+    var change = startingAmount % costPerBottle;    
+    
+    return change;
+}
+
+// 1 Bottle = $ 1.5
+
+console.log("Hello master, here is your " + getMilk(5, 2.5) + " change.");
 
 
 
@@ -109,3 +126,46 @@ getMilk(10);
 // }
 
 
+// function lifeInWeeks(age) {
+    
+// /************Don't change the code above************/    
+    
+//     //Write your code here.
+//     var year = 90 - age;
+//     var month = year * 12;
+//     var weeks = year * 52;
+//     var days = year * 365;
+//     console.log("You have " + days + " days, " + weeks + " weeks, and " + month + " months left." )
+    
+    
+    
+// /*************Don't change the code below**********/
+// }
+
+// lifeInWeeks(56);
+
+
+
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+
+// function bmiCalculator(weight,height) {
+//     return Math.floor(weight/(height * height));
+// }
+
+// function bmiCalculator(weight,height) {
+//     return Math.floor(weight/Math.pow(height,2));
+// }
+
+// function bmiCalculator(weight,height) {
+//     return Math.round(weight/Math.pow(height,2));
+// }
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+console.log(bmi); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
